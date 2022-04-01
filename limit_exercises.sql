50090883a7a9e1491d5d276767b09657095f35ab
Lincoln Muriithi <lincoln.muriithi11@gmail.com>
2022-03-31 22:28:50 -0500

    Complete limit exercises

---
+++ limit_exercises.sql
@@ -0,0 +1,38 @@
+USE employees;
+-- 2. List the first 10 distinct last name sorted in descending order.
+SELECT DISTINCT
+    last_name
+FROM
+    employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+-- 3. Find all previous or current employees hired in the 90s and born on Christmas. Find the first 5 employees hired in the 90's by sorting by hire date and limiting your results to the first 5 records. Write a comment in your code that lists the five names of the employees returned.
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    hire_date LIKE '199%'
+        AND birth_date LIKE '%-12-25'
+ORDER BY hire_date
+LIMIT 5;
+-- 'Alselm','Cappello'
+-- 'Utz','Mandell'
+-- 'Bouchung','Schreiter'
+-- 'Baocai','Kushner'
+-- 'Petter','Stroustrup'
+
+-- 4. LIMIT and OFFSET can be used to create multiple pages of data. What is the relationship between OFFSET (number of results to skip), LIMIT (number of results per page), and the page number?
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    hire_date LIKE '199%'
+        AND birth_date LIKE '%-12-25'
+ORDER BY hire_date
+LIMIT 5 OFFSET 45;
+
+
